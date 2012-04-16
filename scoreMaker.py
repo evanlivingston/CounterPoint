fa3db83e16ed22a15d908ffdc630f897211970a0
Evan Livingston <evanlivingston@gmail.com>
2012-04-16 12:42:48 -0400

    made readme more readable

--- scoreMaker.py
+++ scoreMaker.py
@@ -1,11 +1,14 @@
 import random
+from subprocess import call
 
 class scoreMaker:
     """Used to creare lilypond files"""
     cantusNotes = []
+    cantusValues = []
     counterNotes = []
-    fileOutName = 'lilyFile_'+ str(random.getrandbits(32)) + '.ly'
     
+    fileOutName = 'lilyFile_'+ str(random.getrandbits(32)) + '.ly'
+    1,2
     
     
 
@@ -15,7 +18,7 @@ class scoreMaker:
     def noteToName(note):
         note = note
         notes = ('c','cis','d','dis','e','f','fis','g','gis','a','ais','b')
-        octaves = (',,,',',,',',','','\'','\'\'','\'\'\'')
+        octaves = (',,,,',',,,',',,',',','','\'','\'\'')
         if (note >= 0) and( note <= 119):
             return (notes[note%12] + octaves[int(note/12)] + '1')
 
@@ -24,35 +27,58 @@ class scoreMaker:
         
         newNote = scoreMaker.noteToName(note)
         scoreMaker.cantusNotes.append('\t\t' + newNote + "\n")
+        scoreMaker.cantusValues.append(note)
 
     def appendCounter(self, note):
         newNote = scoreMaker.noteToName(note)
         scoreMaker.counterNotes.append('\t\t' + newNote + "\n")
         
 
-    def write(self):
+    def write():
         fileOut = open(scoreMaker.fileOutName, "w+")
-        fileOut.write('\\version \"2.15.36\"\n')
         fileOut.write('\\score {\n')
-        fileOut.write('\t<< \n')
-        fileOut.write('\t\t\\new Staff {\n')
-        fileOut.write('\t\t\\clef treble\n')
-        fileOut.write('\t\t{\n')
+        fileOut.write('\t\\new Staff << \n')
+        fileOut.write('\t\\new Voice {\n')
+        fileOut.write('\t\\set midiInstrument = #\"clarinet"\n')
+        fileOut.write('\t\\time 4/4\n')
+        fileOut.write('\t\\voiceOne\n')
+        fileOut.write('\t\t\\clef treble {\n')
         for x in scoreMaker.counterNotes:
             fileOut.write(str(str(x)))
         fileOut.write('\t\t}\n')
         fileOut.write('\t}\n')
-        fileOut.write('\t\t\\new Staff {\n')
-        fileOut.write('\t\t\\clef tenor\n')
-        fileOut.write('\t\t{\n')
+        fileOut.write('\t\t\\new Staff\n')
+        fileOut.write('\t\\new Voice {\n')
+        fileOut.write('\t\\set midiInstrument = #\"clarinet"\n')
+        if sum(scoreMaker.cantusValues) > 60:
+            fileOut.write('\t\t\\clef bass {\n')
+        else:
+            fileOut.write('\t\t\\clef bass {\n')
+        fileOut.write('\t\\voiceTwo\n')
         for x in scoreMaker.cantusNotes:
             fileOut.write(str(str(x)))
         fileOut.write('\t\t}\n')
         fileOut.write('\t}\n')
         fileOut.write('>>\n')
-        fileOut.write('}\n')
-        print('file was written')
+        fileOut.write('\\layout {}\n')
+        fileOut.write('\\midi {\n')
+        fileOut.write('\\context {\n')
+        fileOut.write('\\Staff \n')
+        fileOut.write('\\remove \"Staff_performer\" \n')
+        fileOut.write('\t}\n')
+        fileOut.write('\\context {\n')
+        fileOut.write('\\Voice \n')
+        fileOut.write('\\consists \"Staff_performer\" \n')
+        fileOut.write('\t}\n')
+        fileOut.write('\\context {\n')
+        fileOut.write('\\Score \n')
+        fileOut.write('tempoWholesPerMinute = #(ly:make-moment 72 2)\n')
+        fileOut.write('\t}\n')
+        fileOut.write('\t}\n')
+        fileOut.write('\t}\n')
+        print('file was written- ' + scoreMaker.fileOutName)
         fileOut.close()
-        
+        z = call(["/Applications/LilyPond.app/Contents/Resources/bin/lilypond", scoreMaker.fileOutName])
+        print(z)
         
 
